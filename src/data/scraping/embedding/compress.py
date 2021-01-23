0461d8c5f0ed97308f604351fd169bc81f974079
Rodolfo De Nadai <rdenadai@gmail.com>
2021-01-23 14:52:29 -0300

    novo loading e missing deps

--- src/data/scraping/embedding/compress.py
+++ src/data/scraping/embedding/compress.py
@@ -2,35 +2,43 @@ import os
 import time
 import warnings
 import codecs
+from functools import partial
+from concurrent.futures import ProcessPoolExecutor
 
 import numpy as np
 import pandas as pd
 from nltk.corpus import machado, mac_morpho, floresta
 
-from ...processing.utils import CleanUp
+from ...processing.utils import CleanUp, divide_chunks
 
 
 warnings.filterwarnings("ignore")
 
 
-def carregar_sentencas(filename):
-    sentences = pd.read_csv(filename, header=None, sep="\\n", iterator=True, chunksize=5000)
-    for sents in sentences:
-        for sent in sents.to_numpy(dtype=str):
-            frase = normalizar.fit(sent[0])
-            if len(frase) >= 5:
-                yield frase
+def load_sentences(tipo, sentence):
+    sentence = sentence[0] if tipo == 0 else " ".join(sentence)
+    sentence = normalizar.fit(sentence)
+    if len(sentence) >= 5:
+        return ' '.join(sentence).strip()
+    return None
 
 
-def corpus_nltk(model):
-    with codecs.open(
-        f"{os.getcwd()}/data/embedding/corpus.txt", "ab", encoding="utf-8"
-    ) as fh:
+def carregar_sentencas(fh, filename):
+    with ProcessPoolExecutor() as exc:
+        sentences = pd.read_csv(filename, header=None, sep="\\n", iterator=True, chunksize=5000)
+        for sents in sentences:
+            sentences_ = list(filter(None, exc.map(partial(load_sentences, 0), sents.to_numpy(dtype=str), chunksize=100)))
+            if len(sentences_) > 0:
+                np.savetxt(fh, sentences_, fmt="%s")
+
+
+def corpus_nltk(fh, model):
+    with ProcessPoolExecutor() as exc:
         for fileid in model.fileids():
-            for sent in model.sents(fileid):
-                sentence = normalizar.fit(" ".join(sent))
-                if len(sentence) >= 5:
-                    np.savetxt(fh, [f"{' '.join(sentence).strip()}"], fmt="%s")
+            for sents in divide_chunks(model.sents(fileid), 5000):
+                sentences_ = list(filter(None, exc.map(partial(load_sentences, 1), sents, chunksize=100)))
+                if len(sentences_) > 0:
+                    np.savetxt(fh, sentences_, fmt="%s")
 
 
 if __name__ == "__main__":
@@ -46,39 +54,39 @@ if __name__ == "__main__":
         return_tokens=True,
     )
 
-    print("Carregando sentenças dos corpus da NLTK...")
-    for md in [machado, mac_morpho, floresta]:
-        corpus_nltk(md)
-
-    filenames = [
-        f"{os.getcwd()}/data/embedding/olhar.txt",
-        f"{os.getcwd()}/data/embedding/uol.txt",
-        f"{os.getcwd()}/data/embedding/g1.txt",
-        f"{os.getcwd()}/data/embedding/r7.txt",
-        f"{os.getcwd()}/data/embedding/elpais.txt",
-        f"{os.getcwd()}/data/embedding/ministerio.txt",
-        f"{os.getcwd()}/data/embedding/frases.txt",
-        f"{os.getcwd()}/data/embedding/livros.txt",
-        f"{os.getcwd()}/data/embedding/textos.txt",
-        f"{os.getcwd()}/data/embedding/wikipedia.txt",
-        f"{os.getcwd()}/data/embedding/fapesp.txt",
-        f"{os.getcwd()}/data/embedding/mundo.txt",
-        f"{os.getcwd()}/data/embedding/bulas.txt",
-        f"{os.getcwd()}/data/embedding/copiados_manualmente.txt",
-        f"{os.getcwd()}/data/embedding/temario_2.txt",
-        f"{os.getcwd()}/data/embedding/frases_outras.txt",
-        f"{os.getcwd()}/data/embedding/por-br_newscrawl_2011_1M-sentences.txt",
-        f"{os.getcwd()}/data/embedding/por_wikipedia_2016_1M-sentences.txt",
-        f"{os.getcwd()}/data/embedding/pt_dedup_part.txt",
-    ]
-
-    print("Carregando sentenças dos corpus criados...")
     with codecs.open(
         f"{os.getcwd()}/data/embedding/corpus.txt", "ab", encoding="utf-8"
     ) as fh:
+        print("Carregando sentenças dos corpus da NLTK...")
+        for md in [machado, mac_morpho, floresta]:
+            corpus_nltk(fh, md)
+
+        filenames = [
+            f"{os.getcwd()}/data/embedding/olhar.txt",
+            f"{os.getcwd()}/data/embedding/uol.txt",
+            f"{os.getcwd()}/data/embedding/g1.txt",
+            f"{os.getcwd()}/data/embedding/r7.txt",
+            f"{os.getcwd()}/data/embedding/elpais.txt",
+            f"{os.getcwd()}/data/embedding/ministerio.txt",
+            f"{os.getcwd()}/data/embedding/frases.txt",
+            f"{os.getcwd()}/data/embedding/livros.txt",
+            f"{os.getcwd()}/data/embedding/textos.txt",
+            f"{os.getcwd()}/data/embedding/wikipedia.txt",
+            f"{os.getcwd()}/data/embedding/fapesp.txt",
+            f"{os.getcwd()}/data/embedding/mundo.txt",
+            f"{os.getcwd()}/data/embedding/bulas.txt",
+            f"{os.getcwd()}/data/embedding/history.txt",
+            f"{os.getcwd()}/data/embedding/copiados_manualmente.txt",
+            f"{os.getcwd()}/data/embedding/temario_2.txt",
+            f"{os.getcwd()}/data/embedding/frases_outras.txt",
+            f"{os.getcwd()}/data/embedding/por-br_newscrawl_2011_1M-sentences.txt",
+            f"{os.getcwd()}/data/embedding/por_wikipedia_2016_1M-sentences.txt",
+            f"{os.getcwd()}/data/embedding/pt_dedup_part.txt",
+        ]
+
+        print("Carregando sentenças dos corpus criados...")
         for filename in filenames:
             print(f"Carregando sentenças: {filename}")
-            for sentence in carregar_sentencas(filename):
-                np.savetxt(fh, [f"{' '.join(sentence).strip()}"], fmt="%s")
+            carregar_sentencas(fh, filename)
 
     print(f"Tempo total da compressao: {round(time.time() - start, 2)}s")
---
+++ src/data/scraping/embedding/history_extractor.py
@@ -0,0 +1,79 @@
+import os
+import codecs
+import asyncio
+import time
+from itertools import chain
+from datetime import datetime
+
+import arrow
+import httpx
+import numpy as np
+from bs4 import BeautifulSoup
+from aiomultiprocess import Pool
+
+
+async def get_link_content(url):
+    phrases = []
+    try:
+        time.sleep(1.5)
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, timeout=240)
+            if r.status_code == 200:
+                html = BeautifulSoup(r.content, "lxml")
+                posts = html.findAll("article", {"class": "hstEntry__content"})
+                for post in posts:
+                    phrases += post.get_text(strip=True).split(".")
+    except Exception as e:
+        print(f"2. Erro ao carregar posts: {url}, {str(e)}")
+    return phrases
+
+
+async def get_links(url):
+    links = []
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, timeout=240)
+            if r.status_code == 200:
+                html = BeautifulSoup(r.content, "lxml")
+                links_ = html.findAll("article", {"class": "block-opacable hstBlock"})
+                for link in links_:
+                    href = link.find("a").get("href")
+                    if "https://history.uol.com.br" not in href:
+                        href = f"https://history.uol.com.br{href}"
+                    links.append(href)
+    except Exception as e:
+        # print(f"2. Erro ao carregar posts: {url}, {str(e)}")
+        pass
+    return links
+
+
+async def carregar(func, urls):
+    async with Pool(processes=4) as pool:
+        result = await pool.map(func, urls)
+    return result
+
+
+if __name__ == "__main__":
+    print("Iniciando History")
+    print("-" * 30)
+    start_date = arrow.get(datetime(2020, 1, 1))
+    links = [f"https://history.uol.com.br/hoje-na-historia/{start_date.shift(days=d).format('YYYY-MM-DD')}" for d in range(1, 366)]
+    
+    links = list(filter(None, chain(*asyncio.run(carregar(get_links, links)))))
+    print(f"Links carregados... {len(links)}")
+    phrases = filter(None, chain(*asyncio.run(carregar(get_link_content, links))))
+    phrases = [phrase.strip() for phrase in phrases if len(phrase) > 15 and not phrase.startswith("Imagem:")]
+
+    try:
+        sentences = []
+        with codecs.open(f"{os.getcwd()}/data/embedding/history.txt", "rb", encoding="utf-8") as fh:
+            sentences = fh.readlines()
+            sentences = [sent.strip() for sent in sentences]
+        with codecs.open(f"{os.getcwd()}/data/embedding/history.txt", "wb", encoding="utf-8") as fh:
+            sents = sorted(list(set(sentences + phrases)))
+            np.savetxt(fh, sents, fmt="%s")
+    except:
+        with codecs.open(f"{os.getcwd()}/data/embedding/history_sec.txt", "wb", encoding="utf-8") as fh:
+            sents = sorted(list(set(phrases)))
+            np.savetxt(fh, sents, fmt="%s")
+    print()
