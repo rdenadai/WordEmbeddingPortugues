87d8141d27e721074df277b959cfea31a91f5c50
Rodolfo De Nadai <rdenadai@gmail.com>
2023-12-24 18:52:45 -0300

    fix: Divide in chunks;

--- src/data/scraping/embedding/super_extractor.py
+++ src/data/scraping/embedding/super_extractor.py
@@ -4,7 +4,7 @@ from itertools import chain
 import httpx
 from bs4 import BeautifulSoup
 
-from .utils import loader, save_phrases
+from .utils import chunks, loader, save_phrases
 
 urls = [f"https://super.abril.com.br/superarquivo/{i}/" for i in range(1, 459)]
 
@@ -19,7 +19,7 @@ async def get_link_content(url):
                 posts = html.findAll("section", {"class": "content"})
                 phrases += list(chain(*[post.get_text().strip().split(".") for post in posts]))
     except Exception as e:
-        print(f"2. Erro ao carregar posts: {url}, {str(e)}")
+        print(f"2. Erro ao carregar content: {url}, {str(e)}")
     return phrases
 
 
@@ -40,9 +40,10 @@ async def get_links(url):
 if __name__ == "__main__":
     print("Iniciando SuperInteressante")
     print("-" * 30)
-    links = list(filter(None, chain(*asyncio.run(loader(get_links, urls)))))
-    print("Links carregados...")
-    phrases = filter(None, chain(*asyncio.run(loader(get_link_content, links))))
-    phrases = [pphrase for phrase in phrases if len(pphrase := phrase.strip()) > 5]
-    save_phrases(phrases, "/data/embedding/mundo.txt")
+    for chunked in chunks(urls, 30):
+        links = list(filter(None, chain(*asyncio.run(loader(get_links, chunked)))))
+        print(f"Links carregados... {len(links)}")
+        phrases = filter(None, chain(*asyncio.run(loader(get_link_content, links))))
+        phrases = [pphrase for phrase in phrases if len(pphrase := phrase.strip()) > 5]
+        save_phrases(phrases, "/data/embedding/mundo.txt")
     print()
